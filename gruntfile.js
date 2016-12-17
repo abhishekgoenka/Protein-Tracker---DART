80a09f830a335aa3c4c9791a4de43de0c534562f
Goenka <Abhishek.Goenka@allscripts.com>
2016-12-17 09:51:23 +0530

    Updated gruntfile and add few required packages

--- gruntfile.js
+++ gruntfile.js
@@ -2,23 +2,56 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
+       // 1.
+        // create build web directory
+        mkdir: {
             build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+                options: {
+                    create: ['build/web']
+                }
             }
         },
+        // 2.
+        // compile dart files
         dart2js: {
             options: {
-                // Task-specific options go here. 
+                // use this to fix a problem into dart2js node module. The module calls dart2js not dart2js.bat.
+                // this is needed for Windows. So use the path to your dart2js.bat file
+                "dart2js_bin": "C:/tools/dart-sdk/bin/dart2js.bat"
             },
-            your_target: {
-                // Target-specific file lists and/or options go here. 
-            },
+            compile: {
+                files: {'build/web/main.dart.js': 'web/main.dart'}
+            }
+        },
+        // 3.
+        // copy all needed files, including all needed packages
+        // except the .dart files.
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            'web/!(*.dart)',
+                            'web/css/*.css',
+                            'web/res/*.svg',
+                            'web/packages/angular/**/!(*.dart)',
+                            'web/packages/browser/**/!(*.dart)'
+                        ],
+                        dest: 'build'
+                    }
+                ]
+            }
+        },
+        // 4.
+        // remove empty directories copied using the previous task
+        cleanempty: {
+            build: {
+                options: {
+                    files: false
+                },
+                src: ['build/web/packages/**/*']
+            }
         },
     });
 
@@ -27,11 +60,16 @@ module.exports = function (grunt) {
 
     // Use the dart2js tool to compile Dart code to JavaScript
     //https://webdev.dartlang.org/tools/dart2js
-    // Load the plugin that will compile Dart code to Ja
-    grunt.loadNpmTasks('grunt-dart2js');
+    // Load the plugin that will compile Dart code to JavaScript
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 
     // Default task(s).
-    grunt.registerTask('default', ["dart"]);
+    grunt.registerTask('default', [
+        'mkdir:build',
+        'dart2js',
+        'copy:build',
+        'cleanempty:build'
+    ]);
 
 };
--- readme.md
+++ readme.md
@@ -1,17 +1,25 @@
 ## Synopsis
-This repository contains Protein Tracker application. This application is developed in [DART](https://www.dartlang.org/). 
-Dart is an application programming language that’s easy to learn, easy to scale, and deployable everywhere.
+This repository contains Protein Tracker application. This application is developed in [DART](https://www.dartlang.org/). Dart is an application programming language that’s easy to learn, easy to scale, and deployable everywhere. Google depends on Dart to make very large apps.
 
-Google depends on Dart to make very large apps.
+## Installing DART
+With [Chocolatey](https://chocolatey.org/), installing Dart is easy. The dart package contains the Dart SDK, which includes the Dart VM, libraries, and command-line Dart tools such as dart, dartanalyzer, pub, and dartdoc. If you are doing web development, you should also add one more tool:
+>[Dartium](https://webdev.dartlang.org/tools/dartium): A special build of Chromium that includes a Dart VM. Use it to interactively test and debug Dart web apps without first compiling them to JavaScript.
 
-## Before you begin
-Install [npm](https://www.npmjs.com/package/npm) and [grunt](http://gruntjs.com/getting-started) if not already installed. Install grunt after npm is installed globally.
+```shell
+choco install dart-sdk
+choco install dartium
+```
+
+## Getting Started
+Install [DART](https://www.dartlang.org/install/windows) followed by [npm](https://www.npmjs.com/package/npm) and [grunt](http://gruntjs.com/getting-started) if not already installed. Install grunt after npm is installed globally.
 >npm install -g grunt-cli
 
 Download all the packages using following commands before you start
->npm update
 
->bower update
+```shell
+npm update
+bower update
+```
 
 ## Run Application
 Use the following command to run the project in your default browser
